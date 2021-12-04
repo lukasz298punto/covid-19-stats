f8b55af3f58363736654dd57255995691ff4e666
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2021-12-04 11:35:03 +0100

    Add storybook.

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import 'App.css';
 import { ApolloProvider } from '@apollo/client';
-import { createApolloClient } from 'utility/api';
+import { createApolloClient } from 'utils/api';
 
 const client = createApolloClient(process.env.REACT_APP_API_KEY as string);
 
---
+++ src/layout/__tests__/Root.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Root from 'layout/Root';
+
+it('renders app', () => {
+    render(<Root />);
+});
