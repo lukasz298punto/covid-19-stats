3e122479dbcbbefb31fce06fe3ccd248f4f71f48
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2021-12-04 16:18:20 +0100

    Add tailiwnd.

--- src/layout/__tests__/Root.test.tsx
+++ src/layout/__tests__/Root.test.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import Root from 'layout/Root';
+import renderer from 'react-test-renderer';
 
 it('renders app', () => {
     render(<Root />);
+});
+
+it('could accept none react element icon', () => {
+    const component = renderer.create(<Root />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
