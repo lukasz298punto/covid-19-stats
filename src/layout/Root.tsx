3e7c83850b33df607b70057153ab15005629219e
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2021-12-04 12:32:10 +0100

    Add AntDesign, configure craco.

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { createApolloClient } from 'utils/api';
+import './style/root.less';
+import { Result } from 'antd';
 
 const client = createApolloClient(process.env.REACT_APP_API_KEY as string);
 
 function App() {
     return (
         <ApolloProvider client={client}>
-            <div className="App">3234</div>
+            <Result status="500" title="500" subTitle="Sorry page under construction." />
         </ApolloProvider>
     );
 }
