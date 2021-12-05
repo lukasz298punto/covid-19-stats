6d8a2deee6f9798e49a5d78d718284fbfedcb134
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2021-12-05 18:22:56 +0100

    Create base layout.

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,17 +1,14 @@
 import { ApolloProvider } from '@apollo/client';
 import { createApolloClient } from 'utils/api';
 import './style/root.less';
-import { Result } from 'antd';
-import { StatCard } from 'components/StatCard';
+import Base from 'layout/Base';
 
 const client = createApolloClient(process.env.REACT_APP_API_KEY as string);
 
 function App() {
     return (
         <ApolloProvider client={client}>
-            <Result status="500" title="500" subTitle="Sorry page under construction." />
-            <StatCard value={12} kind="error" />
-            <div className="stat-card-info">11</div>
+            <Base />
         </ApolloProvider>
     );
 }
